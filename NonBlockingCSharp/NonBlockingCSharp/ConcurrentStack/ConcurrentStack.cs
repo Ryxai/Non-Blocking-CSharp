1610faf5244682376e1ecf0d961c2441526a42af
Riyad Parvez <social.riyad@gmail.com>
2013-07-01 00:23:37 +0600

    Added concurrent stack

--- NonBlockingCSharp/NonBlockingCSharp/ConcurrentStack/ConcurrentStack.cs
+++ NonBlockingCSharp/NonBlockingCSharp/ConcurrentStack/ConcurrentStack.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NonBlockingCSharp.Utilities;
 
 namespace NonBlockingCSharp.ConcurrentStack
 {
@@ -14,14 +15,44 @@ namespace NonBlockingCSharp.ConcurrentStack
 
         public ConcurrentStack()
         {
-            head = new Node<T>();
+            head = new Node<T>(default(T));
+        }
+
+        public bool TryPop(out T result)
+        {
+            Node<T> node;
+            do
+            {
+                node = head.next;
+                if (node == null)
+                {
+                    result = default(T);
+                    return false;
+                }
+            } while (!AtomicOperationsUtilities.CompareAndSwap(ref head.next, node, node.next));
+            result = node.item;
+            return true;
+        }
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            do
+            {
+                node.next = head.next;
+            } while (!AtomicOperationsUtilities.CompareAndSwap(ref head.next, node.next, node));
         }
 
         [Serializable]
         private class Node<T>
         {
-            private Node<T> next;
-            private T item;
+            public Node<T> next;
+            public T item;
+
+            public Node(T item) 
+            {
+                this.item = item;
+            }
         }
     }
 }
