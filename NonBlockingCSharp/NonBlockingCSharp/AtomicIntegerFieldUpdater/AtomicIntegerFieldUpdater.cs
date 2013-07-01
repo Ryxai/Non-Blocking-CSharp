0fd5e63bedbb946102eef02b78189b6afbcfd5eb
Riyad Parvez <social.riyad@gmail.com>
2013-07-01 11:44:38 +0600

    Added atomic integer field updater

---
+++ NonBlockingCSharp/NonBlockingCSharp/AtomicIntegerFieldUpdater/AtomicIntegerFieldUpdater.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonBlockingCSharp.AtomicIntegerFieldUpdater
+{
+    public class AtomicIntegerFieldUpdater<T>
+    {
+        public static AtomicIntegerFieldUpdater<T> 	NewUpdater(Type type, string fieldName)
+        {
+
+        }
+
+        /// <summary>
+        /// Get all the fields of a class
+        /// </summary>
+        /// <param name="type">Type object of that class</param>
+        /// <returns></returns>
+        private static IList<FieldInfo> GetAllFields(this Type type)
+        {
+            if (type == null)
+            {
+                return Enumerable.Empty<FieldInfo>().ToList();
+            }
+
+            BindingFlags flags = BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Static |
+            BindingFlags.Instance |
+            BindingFlags.DeclaredOnly;
+            return type.GetFields(flags).Union(GetAllFields(type.BaseType)).ToList();
+        }
+
+        /// <summary>
+        /// Get all properties of a class
+        /// </summary>
+        /// <param name="type">Type object of that class</param>
+        /// <returns></returns>
+        private static IList<PropertyInfo> GetAllProperties(this Type type)
+        {
+            if (type == null)
+            {
+                return Enumerable.Empty<PropertyInfo>().ToList();
+            }
+            BindingFlags flags = BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Static |
+            BindingFlags.Instance |
+            BindingFlags.DeclaredOnly;
+
+            return type.GetProperties(flags).Union(GetAllProperties(type.BaseType)).ToList();
+        }
+    }
+}
