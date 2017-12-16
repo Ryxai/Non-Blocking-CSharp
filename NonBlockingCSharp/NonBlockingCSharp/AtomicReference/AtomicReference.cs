82075effd0f8779d5de716a475b673e6bf5d061f
Jon Baum <jmichaelbaum@gmail.com>
2017-12-16 07:36:13 -0500

    Added Reference and Reference Array

---
+++ NonBlockingCSharp/NonBlockingCSharp/AtomicReference/AtomicReference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NonBlockingCSharp.Utilities;
+
+namespace NonBlockingCsharp.AtomicReference
+{
+    public class AtomicReference<T> where T : class
+    {
+        private T item;
+
+        public T reference
+        {
+            get => item;
+            set => Interlocked.Exchange(ref item, value);
+        }
+
+        public AtomicReference(T initialRef)
+        {
+            Interlocked.Exchange(ref item, initialRef);
+        }
+
+        public Boolean CompareAndSet(T expectedReference, T newReference)
+        {
+            return AtomicOperationsUtilities.CompareAndSwap(ref item, expectedReference, newReference);
+        }
+    }
+}
---
+++ NonBlockingCSharp/NonBlockingCSharp/AtomicReferenceArray/AtomicReferenceArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NonBlockingCSharp.Utilities;
+
+namespace NonBlockingCsharp.AtomicReferenceArray
+{
+    class AtomicReferenceArray<T> where T : class
+    {
+        private T[] items;
+
+        public T this[int key] {
+            get => items[key];
+            set => Interlocked.Exchange(ref items[key], value);
+        }
+
+        public int Length => items.Length;
+
+        public bool CompareAndSet(int index, T expectedReference, T newReference)
+        {
+            return 
+                AtomicOperationsUtilities.CompareAndSwap(ref items[index],
+                                             expectedReference, newReference);
+        }
+
+        public AtomicReferenceArray(int length)
+        {
+            items = new T[length];
+            if (length > 0)
+                Interlocked.Exchange(ref items[0], null);
+        }
+
+        public AtomicReferenceArray(T[] array)
+        {
+            if (array == null)
+                throw new NullReferenceException();
+            int length = array.Length;
+            items = new T[length];
+            if (length > 0)
+            {
+                for (int i = 0; i < length; i++)
+                    Interlocked.Exchange(ref items[i], array[i]);
+            }
+        }
+    }
+}
